abb505de71c4a26dceee9b65f10e20c84ffed0af
Medida Nagaraju <nagarajumedida123@gmail.com>
2022-07-21 19:50:43 +0530

    Homepage

--- HomeNavbar/index.js
+++ HomeNavbar/index.js
@@ -70,4 +70,141 @@ nagaraju(CompaniesNames);
     })
  };
  
+ let exploreCompany = [
+        {
+            title:"MNCs",
+            para:"1.3K+ are actively hiring",
+            img1:"https://img.naukimg.com/logo_images/groups/v1/869714.gif",
+            img2:"https://img.naukimg.com/logo_images/groups/v1/233026.gif",
+            img3:"https://img.naukimg.com/logo_images/groups/v1/52702.gif",
+            img4:"https://img.naukimg.com/logo_images/groups/v1/235458.gif"
+        },
+        {
+            title:"Fortune 500",
+            para:"84 are actively hiring",
+            img1:"https://img.naukimg.com/logo_images/groups/v1/3208.gif",
+            img2:"https://img.naukimg.com/logo_images/groups/v1/255972.gif",
+            img3:"https://img.naukimg.com/logo_images/groups/v1/142780.gif",
+            img4:"https://img.naukimg.com/logo_images/groups/v1/1320206.gif"
+
+        },
+        {
+            title:"Unicorns",
+            para:"67 are actively hiring",
+            img1:"https://img.naukimg.com/logo_images/groups/v1/1879652.gif",
+            img2:"https://img.naukimg.com/logo_images/groups/v1/1205138.gif",
+            img3:"https://img.naukimg.com/logo_images/groups/v1/1221608.gif",
+            img4:"https://img.naukimg.com/logo_images/groups/v1/1194340.gif"
+
+        },
+        {
+            title:"Startups",
+            para:"185 are actively hiring",
+            img1:"https://img.naukimg.com/logo_images/groups/v1/2519816.gif",
+            img2:"https://img.naukimg.com/logo_images/groups/v1/3059420.gif",
+            img3:"https://img.naukimg.com/logo_images/groups/v1/4599509.gif",
+            img4:"https://img.naukimg.com/logo_images/groups/v1/1694498.gif"
+        }
+       
+ ];
+
+
+ nagaraju1(exploreCompany);
+ function nagaraju1(data){
+    data.forEach(function(ele){
+        let dabba = document.createElement("div");
+
+        let title = document.createElement("h3");
+        title.innerText = ele.title;
+
+        let para = document.createElement("p");
+        para.innerText = ele.para;
+
+        let img1 = document.createElement("img");
+        img1.setAttribute("src",ele.img1);
+
+        let img2 = document.createElement("img");
+        img2.setAttribute("src",ele.img2);
+
+        let img3 = document.createElement("img");
+        img3.setAttribute("src",ele.img3);
+
+        let img4 = document.createElement("img");
+        img4.setAttribute("src",ele.img4);
+
+        dabba.append(title,para,img1,img2,img3,img4);
+        document.querySelector("#eight").append(dabba)
+    })
+ };
+
+
+ let featuredCompany=[
+    {
+        img1:"https://img.naukimg.com/logo_images/groups/v2/240936.gif",
+        title:"JPMorgan Chase Bank",
+        rating:"⭐4.2 (3.4K+ reviews)",
+        para:"Leader in financial services.",
+        btn:"View jobs",
+    },
+    {
+        img1:"https://img.naukimg.com/logo_images/groups/v2/9558.gif",
+        title:"Nagarro",
+        rating:"⭐4.4 (2.2K+ reviews)",
+        para:"Leader in digital product engineering.",
+        btn:"View jobs"
+    },
+    {
+        img1:"https://img.naukimg.com/logo_images/groups/v2/13832.gif",
+        title:"Infosys",
+        rating:"⭐4.0 (25.8K+ reviews)",
+        para:"Global leader in next-gen digital services",
+        btn:"View jobs"
+    },
+    {
+        img1:"https://img.naukimg.com/logo_images/groups/v2/4156.gif",
+        title:"Cognizant",
+        rating:"⭐4.0 (26.8K+ reviews)",
+        para:"Leading ITeS company with global presence.",
+        btn:"View jobs"
+    },
+    {
+        img1:"https://img.naukimg.com/logo_images/groups/v2/41608.gif",
+        title:"Wipro",
+        rating:"⭐3.9 (25.5K+ reviews)",
+        para:"Leading technology services & consulting.",
+        btn:"View jobs"
+
+    }
+ ];
+
+ nagaraju2(featuredCompany);
+ 
+ function nagaraju2(data){
+  
+    data.forEach(function(ele){
+
+        let dabba = document.createElement("div");
+        
+
+        let one = document.createElement("img");
+        one.setAttribute('src',ele.img1);
+
+        let two = document.createElement("h2");
+        two.innerText = ele.title;
+
+        let three = document.createElement("p");
+        three.innerText = ele.rating;
+
+        let four = document.createElement("h5");
+        four.innerText = ele.para;
+
+        let five = document.createElement("h4");
+        five.innerText = ele.btn;
+        
+
+        dabba.append(one,two,three,four,five);
+        document.querySelector("#ten").append(dabba);
+    })     
+ }
+
 
