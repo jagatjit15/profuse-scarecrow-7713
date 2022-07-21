d0498dffe52370e05e77c6eef475c892afcc1805
jagatjit barik <itsjagatjitbarik@gmail.com>
2022-07-21 19:30:50 +0530

    all completed

--- src/employee.js
+++ src/employee.js
@@ -1,8 +1,35 @@
+// importing from export file
+
+import { inputField, captcha } from "../export/salesInq.js";
+let inputfield = document.getElementById("inputfield");
+inputfield.innerHTML = inputField();
+
+let footer_form = document.getElementById("footer_form");
+footer_form.innerHTML = inputField();
+
+let captcha_sec = document.getElementById("captcha_sec");
+captcha_sec.innerHTML = captcha();
+
+let captcha_sec2 = document.getElementById("captcha_sec2");
+captcha_sec2.innerHTML = captcha();
+
 // Recruitement call back
 let btn = document.getElementById("clback");
 btn.addEventListener("click", callBack);
 function callBack() {
   btn.innerText = "We will call you back";
+  document.querySelector("#name").value = "";
+  document.querySelector("#company_name").value = "";
+  document.querySelector("#contact").value = "";
+  document.querySelector("#designation_name").value = "";
+  document.querySelector("#mail_Id").value = "";
+  document.querySelector("#city").value = "";
+}
+
+let btn2 = document.getElementById("clback2");
+btn2.addEventListener("click", callBack2);
+function callBack2() {
+  btn2.innerText = "We will call you back";
   document.querySelector("#name").value = "";
   document.querySelector("#company_name").value = "";
   document.querySelector("#contact").value = "";
